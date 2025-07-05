60b9a04554ed4ff58565d334c9a7b1361d17b306
EmilioRL <ejruizlinares@gmail.com>
2025-07-05 18:18:15 +0200

    Ejercicios realizados

--- IntroBBDD.sql
+++ IntroBBDD.sql
@@ -4,3 +4,28 @@
 SELECT flight_id, flight_no, status
 FROM flights
 WHERE status = 'On Time';
+
+
+-- EJERCICIO 2
+
+SELECT * 
+FROM bookings
+WHERE total_amount >= 1000000;
+
+
+-- EJERCICIO 3
+
+SELECT *
+FROM aircrafts_data;
+
+-- EJERCICIO 4
+
+SELECT flight_id
+FROM FLIGHTS
+WHERE aircraft_code = '733';
+
+-- EJERCICIO 5
+
+SELECT *
+FROM tickets
+WHERE passenger_name LIKE 'IRINA%';
